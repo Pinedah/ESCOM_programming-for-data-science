79798b0ef0c13af80c59b69421d12e3cc9723d36
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-07 11:48:23 -0600

    Update insertion-sort.py
    add function to generate random numbers

--- insertion-sort.py
+++ insertion-sort.py
@@ -10,6 +10,9 @@ def insertion_sort(arr):
             i -= 1
         arr[i + 1] = key
 
+def generar_numeros_aleatorios(cantidad):
+    return [random.randint(1, 100000) for _ in range(cantidad)]
+
 numbers = [1,2,754,32,54,2,8,2,3,53,23,8,5,6]
 
 insertion_sort(numbers)
