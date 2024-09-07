4647223f5845e382ed1b1b38177a7305388edc52
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-07 11:46:59 -0600

    Update insertion-sort.py
    create test list and correct for iterations

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,11 +1,19 @@
 #! python3 
 # insertion-sort.py - 
 
+import random 
+
 def insertion_sort(arr):
-    for j in range(1, len(arr) - 1):
+    for j in range(1, len(arr)):
         key = arr[j]
         i = j - 1
         while i >= 0 and arr[i] > key:
             arr[i + 1] = arr[i]
             i -= 1
         arr[i + 1] = key
+
+numbers = [1,2,754,32,54,2,8,2,3,53,23,8,5,6]
+
+insertion_sort(numbers)
+
+print((numbers))
