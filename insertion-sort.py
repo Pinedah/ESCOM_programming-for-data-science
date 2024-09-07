c353ef6eec7932c557e61ef433b525e954897164
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-07 11:48:49 -0600

    Update insertion-sort.py
    import random

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,6 +1,8 @@
 #! python3 
 # insertion-sort.py - 
 
+import random
+
 def insertion_sort(arr):
     for j in range(1, len(arr)):
         key = arr[j]
@@ -10,8 +12,8 @@ def insertion_sort(arr):
             i -= 1
         arr[i + 1] = key
 
-def generar_numeros_aleatorios(cantidad):
-    return [random.randint(1, 100000) for _ in range(cantidad)]
+def random_list(size):
+    return [random.randint(1, 100000) for _ in range(size)]
 
 numbers = [1,2,754,32,54,2,8,2,3,53,23,8,5,6]
 
