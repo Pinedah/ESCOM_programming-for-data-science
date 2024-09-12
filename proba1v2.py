c48f61b08431be354df5d8331cf12c7509b4dc29
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-12 13:12:59 -0600

    si

--- proba1v2.py
+++ proba1v2.py
@@ -6,9 +6,9 @@ def trapecios(n, mu=158, sigma=4):
     return (1 / (sigma * math.sqrt(2 * math.pi))) * math.exp(-((n - mu)**2) / (2 * sigma**2))
 
 # Parámetros
-inicio = 160    # Inicio del intervalo
-fin = 200       # Fin del intervalo
 paso = 0.0008   # Paso de integración (más pequeño para mayor precisión)
+inicio = 160 + paso    # Inicio del intervalo
+fin = 200  - paso     # Fin del intervalo
 area = 0        # Inicializar el área
 
 # Sumar las áreas bajo la curva usando la regla del trapecio
@@ -16,6 +16,10 @@ while inicio <= fin:
     print(f"{inicio}\t-\t{area}")
     area += trapecios(inicio) * paso
     inicio += paso
+else:
+    area *= 2
+
+area = (area + trapecios(160) + trapecios(200)) * paso 
 
 # Imprimir el área total bajo la curva en el intervalo dado
 print("Área bajo la curva (normal general):", area)
