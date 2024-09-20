d549178b9df62ec5586e2c414a67d00e54702116
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-20 08:05:01 -0600

    Update quick-sort.py

--- quick-sort.py
+++ quick-sort.py
@@ -1,34 +1,32 @@
 import random
-
-def exchange(arr,i,j):
-    temp=arr[i]
-    arr[i]=arr[j]
-    arr[j]=temp
-
-def partition(arr,p,r):
-    x=arr[r]
+def exchange(A,i,j):
+    temp=A[i]
+    A[i]=A[j]
+    A[j]=temp
+def partition(A,p,r):
+    x=A[r]
     i=p-1
     for j in range(p,r):
-        if arr[j]<=x:
+        if A[j]<=x:
             i=i+1
-            exchange(arr,i,j)
-    exchange(arr,i+1,r) 
+            exchange(A,i,j)
+    exchange(A,i+1,r) 
     return i+1   
-
-def quickSort(arr,p,r):
+def quickSort(A,p,r):
     if p<r:
-        q=partition(arr,p,r)
-        quickSort(arr,p,q-1)
-        quickSort(arr,q+1,r)
+        q=partition(A,p,r)
+        quickSort(A,p,q-1)
+        quickSort(A,q+1,r)
 
 def main():
-
-    size = 100
-    arr = [random.randint(1,100) for i in range(size)]
+    n=10000
+    A=list(range(100000))
     print('Quicksort')
-    print(f'Arreglo desordenado: {arr}')
-    quickSort(arr,0,len(arr)-1)
-    print(f'Arreglo ordenado {arr}')
+    #print(f'Arreglo desordenado: {A}')
+    quickSort(A,0,len(A)-1)
+    #print(f'Arreglo ordenado {A}')
+    print(f'Arreglo ordenado')
 
-if __name__ == '__main_':
+
+if __name__ =='__main__':
     main()
