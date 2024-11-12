f2ca05607cc79fda4baa8833295da55172cd9057
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-11-12 07:56:26 -0600

    acomodarlos en folders

---
+++ bckrm.py
@@ -0,0 +1,11 @@
+
+from rembg import remove 
+from PIL import Image 
+
+input_path = ''
+output_path = ''
+
+inp = Image.open(input_path)
+output = remove(inp)
+output.save(output_path)
+Image.open("a.png")
